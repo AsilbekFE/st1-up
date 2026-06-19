5ea527ac1c529f84822cf96b265d33652947fa9d
asil596 <daminovanvar475@gmail.com>
2026-06-19 15:55:45 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import SectionUniversity from "./components/SectionUniversity";
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <main>
-
+        <Home/>
+        <SectionUniversity/>
       </main>
       <footer>footer</footer>
     </div>
